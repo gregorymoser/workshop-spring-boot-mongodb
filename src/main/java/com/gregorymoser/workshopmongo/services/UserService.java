7b7d87d207ef99d1ccb0ac76f3cfc31b871fab56
Gregory Moser <gdc.moser@hotmail.com>
2020-06-04 17:20:31 +0100

    Getting a user by id

--- src/main/java/com/gregorymoser/workshopmongo/services/UserService.java
+++ src/main/java/com/gregorymoser/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.gregorymoser.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gregorymoser.workshopmongo.domain.User;
 import com.gregorymoser.workshopmongo.repository.UserRepository;
+import com.gregorymoser.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List <User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
